b4fd94deda0f3082ba9461f8c076c9bcacef802b
dihuynh@outlook.sg <dihuynh@outlook.sg>
2024-07-25 00:23:49 +0700

    feat:  connect to db, config env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { MongooseModule } from '@nestjs/mongoose'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    // MongooseModule.forRoot('mongodb+srv://20110246:PLHsM3sAujrylVfb@it-job-db.cyjf9fc.mongodb.net/'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
