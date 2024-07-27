2c79c5664be70eff939d57e30b0fa4e1c4a0bdec
dihuynh@outlook.sg <dihuynh@outlook.sg>
2024-07-28 01:04:51 +0700

    fix: add user into creating company

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -4,13 +4,18 @@ import { UpdateCompanyDto } from './dto/update-company.dto'
 import { SoftDeleteModel } from 'soft-delete-plugin-mongoose'
 import { Company, CompanyDocument } from './schemas/company.schema'
 import { InjectModel } from '@nestjs/mongoose'
+import { IUser } from 'src/users/user.interface'
 
 @Injectable()
 export class CompaniesService {
   constructor(@InjectModel(Company.name) private companyModel: SoftDeleteModel<CompanyDocument>) {}
-  async create(createCompanyDto: CreateCompanyDto) {
+  async create(createCompanyDto: CreateCompanyDto, user: IUser) {
     return await this.companyModel.create({
       ...createCompanyDto,
+      createdBy: {
+        _id: user._id,
+        email: user.email,
+      },
     })
   }
 
