c7a5c64ee50f7861b0e858a5536b3a95f4b90bb8
dihuynh@outlook.sg <dihuynh@outlook.sg>
2024-07-28 16:33:31 +0700

    feat: create RegisterUserDto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -39,4 +39,23 @@ export class CreateUserDto {
   company: Company
 }
 
-export class RegisterUserDto {}
+export class RegisterUserDto {
+  @IsEmail({}, { message: 'Email không đúng định dạng!' })
+  @IsNotEmpty({ message: 'Email không được để trống!' })
+  email: string
+
+  @IsNotEmpty({ message: 'Password không được để trống!' })
+  password: string
+
+  @IsNotEmpty({ message: 'Name không được để trống!' })
+  name: string
+
+  @IsNotEmpty({ message: 'Age không được để trống!' })
+  age: number
+
+  @IsNotEmpty({ message: 'Gender không được để trống!' })
+  gender: string
+
+  @IsNotEmpty({ message: 'Address không được để trống!' })
+  address: string
+}
